b6eed38557287bbb7ebfb52d6602df1fb934e8f6
Protsiuk-Prits Dmytro <78557776+ProtsyukPrits@users.noreply.github.com>
2022-09-09 21:24:04 +0200

    added trended-fetch

---
+++ src/js/apies.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const API_KEY = '68dd2d07f1b8d9799366e4d9411e689b';
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const page = 1;
+
+async function trendingFetch() {
+  const { data }  = await axios.get(`${BASE_URL}trending/all/day?api_key=${API_KEY}`)
+    return data
+}
+
+
+// Сюди додавайте ваші функції і незабудьте змінити ще import
+export { trendingFetch }
