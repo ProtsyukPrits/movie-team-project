b59664486a37370dc47c303a37f7d8eb7ef36893
Volodymyr V <velch83@gmail.com>
2022-09-11 02:15:09 +0300

    pagination html and style added

--- src/js/index.js
+++ src/js/index.js
@@ -3,17 +3,21 @@
 // =========____ЯКЩО КОД ЗАКРУЧЕНИЙ КОМЕНТУЄМО ВСЕ____==========
 import { trendingFetch } from './apies';
 import { cardsMarkup } from './movie-cards-markup';
-
+import Pagination from 'tui-pagination';
 // Наш реф по якому ми звертаємось!
 const gallery = document.querySelector('.movies__gallery');
-
+const container = document.getElementById('pagination');
 // Тут додаємо ваші глобальні змінні
 let items = [];
 // Тут додаємо слухачі подій
 
-
-
 // ========_____Пишемо сюди основні функції_____===============
+var pagination = new Pagination(container, {
+  totalItems: 500,
+  itemsPerPage: 10,
+  visiblePages: 5,
+  centerAlign: true,
+});
 
 // Функція для виклику карток за популярним рейтингом
 async function render() {
