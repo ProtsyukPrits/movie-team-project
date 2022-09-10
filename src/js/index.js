e9239ba9e7136cafd0d13cd03528db362b011b5f
Protsiuk-Prits Dmytro <78557776+ProtsyukPrits@users.noreply.github.com>
2022-09-10 16:58:08 +0200

    processed files

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,26 @@
 // поки що зшиваємо тут, пізніше будемо все впорядковувати
 
 // =========____ЯКЩО КОД ЗАКРУЧЕНИЙ КОМЕНТУЄМО ВСЕ____==========
-import { trendingFetch } from './apies'
+import { trendingFetch } from './apies';
+import { cardsMarkup } from './movie-cards-markup';
+
+// Наш реф по якому ми звертаємось!
+const gallery = document.querySelector('.movies__gallery');
+
+// Тут додаємо ваші глобальні змінні
+
+// Тут додаємо слухачі подій
+
+
+
+// ========_____Пишемо сюди основні функції_____===============
+
+// Функція для виклику карток за популярним рейтингом
+async function render() {
+  const data = await trendingFetch();
+  items = data.results;
+  // console.log('items', items);
+  const createGAl = cardsMarkup(items);
+  gallery.innerHTML = createGAl;
+}
+render();
--- src/js/js-test/render-by-search-query.js
+++
@@ -1,8 +0,0 @@
-// import movieCardTpl from '../templates/movieCardTpl.hbs';
-
-// const element = document.querySelector('.header__search-input');
-// console.log(element);
-
-// export default function renderHomeGalleryBySearchQuery(element) {
-//   element.innerHTML = movieCardTpl();
-// }
