424961a77fec0beb956c64c74f1cafa1407c8823
Stanislaw Grygoriew <stanislaw.grygoriew@gmail.com>
2022-09-10 18:01:26 +0200

    finished modal functionality

--- src/js/login-signup-modal.js
+++ src/js/login-signup-modal.js
@@ -5,6 +5,7 @@ refs = {
   signupForm: document.querySelector('.signup-form'),
   showLoginFormBtn: document.querySelector('.login-show-btn'),
   loginModal: document.querySelector('.login-modal-backdrop'),
+  closeModalBtn: document.querySelector('.login-modal-close-btn'),
 };
 
 refs.loginBtn.addEventListener('click', openLoginModal);
@@ -13,6 +14,8 @@ refs.showLoginFormBtn.addEventListener('click', showLoginForm);
 
 function openLoginModal() {
   refs.loginModal.classList.remove('is-hidden');
+
+  setTimeout(addDocumentClickListener, 200);
 }
 function showSignupForm() {
   refs.loginForm.style.display = 'none';
@@ -21,4 +24,29 @@ function showSignupForm() {
 function showLoginForm() {
   refs.loginForm.style.display = 'block';
   refs.signupForm.style.display = 'none';
+}
+
+function closeLoginModal(e) {
+  if (
+    e.target.closest('.login-modal-close-btn') ||
+    !e.target.closest('.login-modal')
+  ) {
+    refs.loginModal.classList.add('is-hidden');
+    document.removeEventListener('click', closeLoginModal);
+    document.removeEventListener('keydown', closeLoginModal);
+    showLoginForm();
+  }
+}
+function closeLoginModalOnEsc(e) {
+  if (e.code === 'Escape') {
+    refs.loginModal.classList.add('is-hidden');
+    document.removeEventListener('click', closeLoginModal);
+    document.removeEventListener('keydown', closeLoginModal);
+    showLoginForm();
+  }
+}
+
+function addDocumentClickListener() {
+  document.addEventListener('click', closeLoginModal);
+  document.addEventListener('keydown', closeLoginModalOnEsc);
 }
