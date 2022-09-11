665b81c894b626bbc9d7f684af0997f29b596332
Orest Maryniuk <98083499+ormar90@users.noreply.github.com>
2022-09-11 12:44:05 +0300

    created start modal-one-film

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,12 @@
 
 // =========____ЯКЩО КОД ЗАКРУЧЕНИЙ КОМЕНТУЄМО ВСЕ____==========
 import { trendingFetch } from './apies';
+import { fetchByID } from './apies';
+
 import { cardsMarkup } from './movie-cards-markup';
+import { modalOneFilmMarkup } from './movie-cards-markup';
+
+
 
 // Наш реф по якому ми звертаємось!
 const gallery = document.querySelector('.movies__gallery');
@@ -10,6 +15,8 @@ const gallery = document.querySelector('.movies__gallery');
 // Тут додаємо ваші глобальні змінні
 
 // Тут додаємо слухачі подій
+
+gallery.addEventListener('click', onClickModalOpen);
 
 
 
@@ -24,3 +31,23 @@ async function render() {
   gallery.innerHTML = createGAl;
 }
 render();
+
+// Функція для рендеру модального вікна ОДНОГО ФІЛЬМУ
+async function renderOneFilmModal (id) {
+  const data = await fetchByID(id);
+  // console.log(data.genres[0].name);
+  gallery.insertAdjacentHTML('afterend', modalOneFilmMarkup(data));
+}
+
+// Функція-колбек для рендеру модалки по кліку
+async function onClickModalOpen(e) {
+  e.preventDefault();
+  const movieId = e.target.dataset.movieCartId;
+  if (!movieId) return;
+
+  await renderOneFilmModal(movieId);
+  // console.log(movieId);
+  const modalEl = document.querySelector('.backdrop');
+  // console.log(modalEl)
+  modalEl.classList.toggle("is-hidden");
+}
