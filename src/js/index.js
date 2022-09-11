09d06b004ddf12325ff416d8403823618c12a37c
Volodymyr V <velch83@gmail.com>
2022-09-11 11:52:51 +0300

    created_pagination

--- src/js/index.js
+++ src/js/index.js
@@ -12,17 +12,21 @@ let items = [];
 // Тут додаємо слухачі подій
 
 // ========_____Пишемо сюди основні функції_____===============
-var pagination = new Pagination(container, {
-  totalItems: 500,
+const pagination = new Pagination(container, {
+  totalItems: 100,
   itemsPerPage: 10,
   visiblePages: 5,
   centerAlign: true,
 });
 
+pagination.getCurrentPage(2);
+console.log(pagination);
+
 // Функція для виклику карток за популярним рейтингом
 async function render() {
   const data = await trendingFetch();
   items = data.results;
+  // console.log(items);
   // console.log('items', items);
   const createGAl = cardsMarkup(items);
   gallery.innerHTML = createGAl;
